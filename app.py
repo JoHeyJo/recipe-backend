b597ccf2c289936afe08c911d0502e0343c6365a
jf <jpf0628@gmail.com>
2024-02-28 17:13:23 -0800

    issue trying to retrive User from db with correct credentials. Cannot access database through IPython

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///sling_it'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = True  # change to False for production
 
 debug = DebugToolbarExtension(app)
 jwt = JWTManager(app)
@@ -37,5 +37,21 @@ def signup():
     try:
        token = UserRepo.signup(user_name, first_name, last_name, password, email)
        return jsonify({"token": token})
+    except BaseException as e:
+       return jsonify({"error": f"Sign up error: {e}"}), 400
+    
+
+@app.post("/login")
+def login():
+    """Validate user credentials"""
+    user_name = request.json["userName"]
+    password = request.json["password"]
+    try:
+       token = UserRepo.authenticate(user_name, password)
+       if token:
+          return jsonify({"token": token})
+       else:
+          return jsonify({"error": "Invalid credentials"}), 401
     except IntegrityError as e:
-       return jsonify({"error": f"Error in signup: {e}"})
+       return jsonify({"error": f"login error: {e}"}), 401
+    
--- repository.py
+++ repository.py
@@ -6,9 +6,10 @@ from sqlalchemy.exc import IntegrityError
 bcrypt = Bcrypt()
 
 class UserRepo():
+    """Handles interactions with User table"""
     @staticmethod
     def signup(user_name, first_name, last_name, email, password):
-        """Sign up user. Hashes password and adds user to system."""
+        """Sign up user. Hashes password and adds user to system. => Token"""
         hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
 
         user = User(
@@ -27,12 +28,24 @@ class UserRepo():
           return token
         except IntegrityError as e:
             db.session.rollback()
-            # print({"error":f"error in UserRepo.signup =>{e}"})
             if "users_user_name_key" in str(e.orig):
-                 return {"error": "This username is already taken."}
+                 raise {"error": "This username is already taken."}
             elif "users_email_key" in str(e.orig):
-                 return {"error": "This email is already registered."}
+                 raise {"error": "This email is already registered."}
             else:
-                 return {"error": "An error occurred during signup."}
+                 raise {"error": "An error occurred during signup."}
+            
+    @staticmethod
+    def authenticate(user_name, password):
+        """Find user with username and password. If cant match with user or pe is wrong return False"""
+
+        user = User.query.filter_by(user_name=user_name)[0]
+        print('****',user)
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                token = create_access_token(identity=user_name)
+                return token
+        return False  
 
 
