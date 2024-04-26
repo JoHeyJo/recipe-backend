e3f86a79ab0c799f634d16224e4df6d05b93aa61
jf <jpf0628@gmail.com>
2024-04-26 16:01:12 -0700

    Create many to many relationship between tables

--- models.py
+++ models.py
@@ -27,6 +27,8 @@ class Recipe(ReprMixin, TableNameMixin, TimestampMixin, db.Model):
     preparation: Mapped[str_255]
     notes: Mapped[str_255]
 
+class 
+
 def connect_db(app):
     """Connect this database to provided Flask app."""
 
