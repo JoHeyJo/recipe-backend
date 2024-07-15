629913abeab0a9b7abcb5955ea88a949607e810c
jf <jpf0628@gmail.com>
2024-07-15 16:56:11 -0700

    added logic to recip_servies. need to test

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from dotenv import load_dotenv
 from flask_cors import CORS
 from exceptions import *
+from services.recipe_services import RecipeService
 # Execute if app doesn't auto update code
 # flask --app app.py --debug run
 
@@ -85,50 +86,11 @@ def login():
 def add_recipe():
     """Consolidates recipe data before calling repo functions. If successful 
     recipes_ingredients record created"""
-    recipe_data = None
-    # User data
-    user_id = request.json["user_id"]
-    book_id = request.json["book_id"]
-
-    # Recipe data
-    recipe = request.json["recipe"]
-    recipe_name = recipe["name"]
-    preparation = recipe["preparation"] or None
-    notes = recipe["notes"] or None
-
-    # Ingredient data
-    ingredients = recipe["ingredients"] or None
-    # ############ RECIPE CREATION ########
-    # First add ingredients if applicable then add recipe
     try:
-        if ingredients: 
-            ingredients_data = IngredientsRepo.add_ingredients(ingredients)
-
-            recipe_data = RecipeRepo.create_recipe(
-                name=recipe_name, preparation=preparation, notes=notes)
-
-            recipe_data['ingredients'] = ingredients_data
-
-            for ingredient in recipe_data['ingredients']:
-                RecipeIngredientRepo.create_recipe(
-                    recipe_id=recipe_data['recipe_id'], 
-                    ingredient_id=ingredient['ingredient_id'],
-                    quantity_amount_id=ingredient['amount_id'], 
-                    quantity_unit_id=ingredient['unit_id'])
-
-            recipe_data = recipe_data
-        else:
-            recipe_data = RecipeRepo.create_recipe(
-                name=recipe_name, preparation=preparation, notes=notes)
-            recipe_data = recipe_data
-
-    except IntegrityError as e:
-        return jsonify({"error": f"adding recipe & ingredients in add_recipe: {e}"}), 400
-    # ############ ADD RECIPE TO BOOK (recipes_books) ########
-    RecipeBookRepo.create_entry(book_id=book_id,recipe_id=recipe_data["recipe_id"])
-    # ############ ADD BOOK TO USER (users_books) ########
-    UserBookRepo.create_entry(user_id=user_id,book_id=book_id)
-    return jsonify(recipe_data)
+        recipe_data = RecipeService.add_recipe(request.json)
+        return jsonify(recipe_data),200
+    except Exception as e:
+        return handle_error(e)
 
 
 @app.post("/add_book")
---
+++ services/recipe_services.py
@@ -0,0 +1,51 @@
+from repository import *
+
+class RecipeService():
+  """Handles recipe view business logic"""
+  @staticmethod
+  def add_recipe(data):
+    recipe_data = None
+    # User data
+    user_id = data["user_id"]
+    book_id = data["book_id"]
+
+    # Recipe data
+    recipe = data["recipe"]
+    recipe_name = recipe["name"]
+    preparation = recipe["preparation"] or None
+    notes = recipe["notes"] or None
+
+    # Ingredient data
+    ingredients = recipe["ingredients"] or None
+    # ############ RECIPE CREATION ########
+    # First add ingredients if applicable then add recipe
+    try:
+        if ingredients:
+            ingredients_data = IngredientsRepo.add_ingredients(ingredients)
+
+            recipe_data = RecipeRepo.create_recipe(
+                name=recipe_name, preparation=preparation, notes=notes)
+
+            recipe_data['ingredients'] = ingredients_data
+
+            for ingredient in recipe_data['ingredients']:
+                RecipeIngredientRepo.create_recipe(
+                    recipe_id=recipe_data['recipe_id'],
+                    ingredient_id=ingredient['ingredient_id'],
+                    quantity_amount_id=ingredient['amount_id'],
+                    quantity_unit_id=ingredient['unit_id'])
+
+            recipe_data = recipe_data
+        else:
+            recipe_data = RecipeRepo.create_recipe(
+                name=recipe_name, preparation=preparation, notes=notes)
+            recipe_data = recipe_data
+
+    except IntegrityError as e:
+        return jsonify({"error": f"adding recipe & ingredients in add_recipe: {e}"}), 400
+    # ############ ADD RECIPE TO BOOK (recipes_books) ########
+    RecipeBookRepo.create_entry(
+        book_id=book_id, recipe_id=recipe_data["recipe_id"])
+    # ############ ADD BOOK TO USER (users_books) ########
+    UserBookRepo.create_entry(user_id=user_id, book_id=book_id)
+    return recipe_data
