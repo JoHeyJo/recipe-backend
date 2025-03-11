95bdd649a6a2e00e64b9802e972d072b395250bf
jf <jpf0628@gmail.com>
2025-03-10 18:03:34 -0700

    wip: error handler

--- decorators/handle_route_errors.py
+++ decorators/handle_route_errors.py
@@ -1,19 +1,3 @@
-# from functools import wraps
-# from flask import jsonify
-# from utils import handle_error
-
-
-# def route_error_handler(func):
-#     """Decorator to handle errors in Flask routes or services."""
-#     print("$$$$$$$$$")
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-#         try:
-#             return func(*args, **kwargs)
-#         except Exception as e:
-#             return handle_error(e)
-#     return wrapper
-
 import traceback
 from functools import wraps
 from flask import jsonify
@@ -28,37 +12,34 @@ def route_error_handler(func):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            return handle_error(e) 
+            error_code = getattr(e.orig, "pgcode", None)
+            return handle_error(e, error_code)
     return wrapper
 
 
-def handle_error(error):
+def handle_error(error, error_code):
     """Dynamically handles all errors based on type."""
 
     error_type = type(error).__name__  # Get original exception type
     error_message = str(error)
-
-    highlight(error,"#")
-    highlight(error_type,"#")
-    highlight(error_message,"#")
-    highlight(type(error),"#")
-
-    # Capture the full traceback and log it
     tb = traceback.format_exc()
-    print("⚠️ ERROR TRACEBACK START ⚠️")
-    print(tb)  # ✅ Logs full traceback
-    print("⚠️ ERROR TRACEBACK END ⚠️")
 
-    if error_type == "KeyError":
-        return jsonify({"error": error_message})
+    print("⚠️⚠️⚠️⚠️⚠️⚠️⚠️ ERROR TRACEBACK START ⚠️⚠️⚠️⚠️⚠️⚠️⚠️")
+    print(tb) 
+    print(error_code)
+    print("⚠️⚠️⚠️⚠️⚠️⚠️⚠️ ERROR TRACEBACK END ⚠️⚠️⚠️⚠️⚠️⚠️⚠️")
 
-    if isinstance(error, TypeError):
+
+    if error_type == "TypeError":
         return jsonify({"error": error_message}), 400
-    elif isinstance(error, IntegrityError):
+    
+    elif error_type == "IntegrityError":
         return jsonify({"error": error_message}), 400
-    elif isinstance(error, ValueError or KeyError):
-        return jsonify({"error": error_message}), 400
-    elif isinstance(error, SQLAlchemyError):
+    
+    elif error_type == "ValueError" or "KeyError":
+        return jsonify({"value error": error_message}), 400
+    
+    elif error_type == "SQLAlchemyError":
         return jsonify({"error": "Database error", "details": error_message}), 500
 
     return jsonify({"error": "Unexpected error", "type": error_type, "details": error_message}), 500
