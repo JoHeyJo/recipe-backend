2a54a8ed085b182b070b70cb9869a3a949998a58
jf <jpf0628@gmail.com>
2025-05-09 10:56:37 -0700

    parameter_store_api can handle fetching secrets and setting them in app

--- utils/set_environment.py
+++ utils/set_environment.py
@@ -1,8 +1,11 @@
 from environment_config import DevelopmentConfig, ProductionConfig, os
+from SDKs.paramater_store_api import fetch_flask_secret, fetch_jwt_secret
 
 def set_environment(app):
 	"""On application load environment is set based on server software"""
 	if "gunicorn" in os.environ.get("SERVER_SOFTWARE", ""):
+		fetch_jwt_secret(app=app)
+		fetch_flask_secret(app=app)
 		app.config.from_object(ProductionConfig)
 		ProductionConfig.config_app(app=app)
 	else:
