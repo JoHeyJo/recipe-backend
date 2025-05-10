f4610d0bec9f37c05b74580b7164cda3218fe490
jf <jpf0628@gmail.com>
2025-05-09 17:22:38 -0700

    DRYed logic to fetch secrets into one function

--- SDKs/parameter_store_api.py
+++ SDKs/parameter_store_api.py
@@ -3,17 +3,20 @@ import boto3
 # Create a Systems Manager client
 ssm = boto3.client('ssm', region_name='us-west-1')
 
-def fetch_flask_secret(app):
+def fetch_secrets(app):
+	"""Centralizes access point to parameter store"""
+
+	# access and FLASK_SECRET_KEY
 	parameter = ssm.get_parameter(Name='FLASK_SECRET_KEY', WithDecryption=True)
 	secret_key_flask = parameter['Parameter']['Value']
 	app.secret_key_flask = secret_key_flask
 
-def fetch_jwt_secret(app):
+ # access and JWT_SECRET_KEY
 	parameter = ssm.get_parameter(Name='JWT_SECRET_KEY', WithDecryption=True)
 	secret_key_jwt = parameter['Parameter']['Value']
 	app.secret_key_jwt = secret_key_jwt
 
-def fetch_jwt_secret(app):
+ # access and DATABASE_URI
 	parameter = ssm.get_parameter(Name='DATABASE_URI', WithDecryption=True)
 	DATABASE_URI = parameter['Parameter']['Value']
 	app.DATABASE_URI = DATABASE_URI
--- utils/set_environment.py
+++ utils/set_environment.py
@@ -1,11 +1,10 @@
 from environment_config import DevelopmentConfig, ProductionConfig, os
-from SDKs.parameter_store_api import fetch_flask_secret, fetch_jwt_secret
+from SDKs.parameter_store_api import fetch_secrets
 
 def set_environment(app):
 	"""On application load environment is set based on server software"""
 	if "gunicorn" in os.environ.get("SERVER_SOFTWARE", ""):
-		fetch_jwt_secret(app=app)
-		fetch_flask_secret(app=app)
+		fetch_secrets(app=app)
 		app.config.from_object(ProductionConfig)
 		ProductionConfig.config_app(app=app)
 	else:
