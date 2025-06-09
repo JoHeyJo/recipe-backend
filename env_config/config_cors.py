a6244545615bb23a83f0aad2f38de955b0b00cef
jf <jpf0628@gmail.com>
2025-06-09 15:43:24 -0700

    Update methods allowed in CORS config

--- env_config/config_cors.py
+++ env_config/config_cors.py
@@ -10,7 +10,7 @@ def configure_cors(app):
         CORS(app,
              origins=app.config["CLIENT_ORIGIN_URL"],
              supports_credentials=True,
-             methods=["GET", "POST", "OPTIONS"],
+             methods=["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
              allow_headers=["Content-Type", "Authorization"])
     else:
         CORS(app, resources={r"/*": {"origins": {os.environ["CLIENT_URL"]}}})
