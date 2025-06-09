e1f17432068d524632a5b0bf62663fe913c5966d
jf <jpf0628@gmail.com>
2025-06-09 15:10:54 -0700

    WIP:refactor how env variables are loaded into process in developement mode

--- environment_config.py
+++ environment_config.py
@@ -1,19 +1,13 @@
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
-from utils.functions import highlight
 
 load_dotenv()
 
 
 class Config:
     """Standard environment configuration"""
-    highlight(os.environ,"#")
-    try:
-        SECRET_KEY = os.environ['SECRET_KEY']
-    except KeyError:
-        raise RuntimeError(
-            "SECRET_KEY environment variable is required but not set.")
+    # SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
@@ -23,16 +17,8 @@ class Config:
 class DevelopmentConfig(Config):
     """Development environment configuration"""
     ENV = "development"
-    try:
-        JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
-    except KeyError:
-        raise RuntimeError(
-            "JWT_SECRET_KEY environment variable is required but not set.")
-    try:
-        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
-    except KeyError:
-        raise RuntimeError(
-            "DATABASE_URI environment variable is required but not set.")
+    # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_ECHO = True
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = True
