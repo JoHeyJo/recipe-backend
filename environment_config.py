ab974233b5dc785dfe655a38fdd7464985c4c724
jf <jpf0628@gmail.com>
2025-06-09 15:15:14 -0700

    DONE: refactoring how env variables are loaded in development

--- environment_config.py
+++ environment_config.py
@@ -7,7 +7,6 @@ load_dotenv()
 
 class Config:
     """Standard environment configuration"""
-    # SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
@@ -17,8 +16,9 @@ class Config:
 class DevelopmentConfig(Config):
     """Development environment configuration"""
     ENV = "development"
-    # JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.environ['SECRET_KEY']
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
     SQLALCHEMY_ECHO = True
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = True
