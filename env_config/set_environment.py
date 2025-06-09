98928f1b943fbe2fd351c21ad85de2849107724b
jf <jpf0628@gmail.com>
2025-06-09 15:32:59 -0700

    housekeeping

--- env_config/set_environment.py
+++ env_config/set_environment.py
@@ -5,10 +5,8 @@ from utils.functions import highlight
 def set_environment(app):
 	"""On application load environment is set based on server software"""
 	if "gunicorn" in os.environ.get("SERVER_SOFTWARE", ""):
-		highlight("Environment PRODUCTIONS","#")
 		fetch_secrets(app=app)
 		app.config.from_object(ProductionConfig)
 	else:
-		highlight("Environment DEVELOPMENT","#")
 		app.config.from_object(DevelopmentConfig)
 
