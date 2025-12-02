2c97e766b6e461c771b30026cc0414dc53419cc9
jf <jpf0628@gmail.com>
2025-12-02 15:41:07 -0800

    explicitly setting origins - no more debug mode

--- env_config/config_cors.py
+++ env_config/config_cors.py
@@ -8,8 +8,7 @@ def configure_cors(app):
     """Dynamically handle configuration of CORS based on environment"""
     if app.config['ENV'] == 'production':
         CORS(app,
-            #  origins=app.config["CLIENT_ORIGIN_URL"],
-             origins="*",
+             origins=app.config["CLIENT_ORIGIN_URL"],
              supports_credentials=True,
              methods=["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
              allow_headers=["Content-Type", "Authorization"])
