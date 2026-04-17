981fe9aba1350132b8f2240ba9bec41d29be4ff4
jf <jpf0628@gmail.com>
2026-04-17 11:04:23 -0700

    WIP: process_shared_book change branch name to better reflect working being done. Working on sharing a book not recipe

--- repository.py
+++ repository.py
@@ -525,23 +525,19 @@ class UserBookRepo():
             raise type(e)(f"RecipeBookRep - query_user_book error:{e}") from e
 
     @staticmethod
-    def query_shared_book(user_id):
+    def query_shared_book(recipient_id):
         """Query for User's "shared recipes" book"""
         try:
-            # query books.book_type with user id (user_id)
-            # I need to see if a user(recipient) has a book in books that has type shared_inbox
-            
             stmt = (
                 db.select(UserBook)
                 .join(Book, UserBook.book_id == Book.id)
                 .where(
-                    UserBook.user_id == user_id,
+                    UserBook.user_id == recipient_id,
                     Book.book_type == BookType.shared_inbox
                 )
             )
 
             user_book = db.session.execute(stmt)
-            highlight("query_shared_book","!")
             return user_book
         except Exception as e:
             raise type(e)(f"UserBookRepo - query_shared_book error:{e}") from e
