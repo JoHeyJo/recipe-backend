c28868d47fc92cc31c717cd8ff50df7e895afe75
jf <jpf0628@gmail.com>
2026-06-02 16:15:18 -0700

    INIT: create route to faciliate copying a recipe this is exclusive to copying a recipe in the sense that it is chanigng owenership. The copy is reassigned to the new owner

--- app.py
+++ app.py
@@ -125,6 +125,12 @@ def add_recipe(authed_user_id, book_id, user_id):
         request={"recipe": request.json}, book_id=book_id, user_id=authed_user_id)
     return jsonify(recipe_data), 200
 
+@app.post("/books/<book_id>/recipes/<recipe_id>")
+@verify_jwt_identity
+@route_error_handler
+def copy_recipe(authed_user_id, book_id, user_id):
+    """Facilitate changing ownership(copying) of shared recipe"""
+
 
 @app.get("/users/<user_id>/books/<book_id>/recipes")
 @verify_jwt_identity
